35d4d75e32a22d1303b905b56d55516ab6616622
wandafeb <91441470+wandafeb@users.noreply.github.com>
2022-11-25 07:46:41 +0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="app">
+        <Header />
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                    <ProductList />
+                </div>
+                <div class="col-md-4">
+                    <Cart />
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Header from "@/components/Header.vue";
+import ProductList from "@/components/ProductList.vue";
+import Cart from "@/components/Cart.vue";
+
+export default {
+    name: "App",
+    components: {
+        Header,
+        ProductList,
+        Cart,
+    },
+};
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+    <nav class="navbar navbar-light bg-light">
+        <div class="container">
+            <span class="navbar-brand mb-0 h1">Navbar</span>
+            <span class="navbar-brand mb-0 h1">Cart ({{ totalQty }})</span>
+        </div>
+    </nav>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+    name: "Header",
+    computed: {
+        ...mapGetters(["cart"]),
+        totalQty () {
+            return this.cart.reduce((a, b) => a + b.qty, 0);
+        }
+    }
+};
+</script>
+
+<style scoped>
+nav {
+    margin-bottom: 50px;
+}
+</style>
